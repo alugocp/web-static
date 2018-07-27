a94d551e3d97675f6baa689c79c241a5a108b545
alugocp <alugocp@aim.com>
2018-07-27 13:18:31 -0400

    Fixed download error

--- index.js
+++ index.js
@@ -2,21 +2,19 @@
 const build=require("./build/build.js");
 const init=require("./init/init.js");
 
-module.exports=function(){
-  // Check for command
-  if(process.argv.length==2){
-    console.log("Usage: static (init|build|push)");
-    process.exit();
-  }
-  const command=process.argv[2];
-  const params=process.argv.slice(3,process.argv.length);
+// Check for command
+if(process.argv.length==2){
+  console.log("Usage: static (init|build|push)");
+  process.exit();
+}
+const command=process.argv[2];
+const params=process.argv.slice(3,process.argv.length);
 
-  // Run proper command
-  if(command=="init"){
-    init(params);
-  }else if(command=="build"){
-    build(params);
-  }else{
-    console.log("Command '"+params[0]+"' not found");
-  }
+// Run proper command
+if(command=="init"){
+  init(params);
+}else if(command=="build"){
+  build(params);
+}else{
+  console.log("Command '"+params[0]+"' not found");
 }
---
+++ static.sh
@@ -0,0 +1,2 @@
+dir="$(npm root -g)"
+node "$dir/web-static" $1 $2
